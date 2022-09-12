eef2e798685e573fc17fd9a0760a6ed9804d4468
Ron Borenstein <8104846+ron-borenstein@users.noreply.github.com>
2022-09-12 20:45:45 +0300

    added docstring to python scripts

--- etl.py
+++ etl.py
@@ -4,18 +4,27 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    '''
+    Loads the data from the S3 buckets into staging tables in redshift
+    '''
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    '''
+    Transforms data from the staging tables and inserts into the analytical tables
+    '''
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    '''
+    Loads the S3 data into the staging tables, then transforms the data and inserts into the analytical tables
+    '''
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
